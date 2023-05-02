e7b5cf7fd580b4ec9c05aeb291a7a6adeeda0755
silkroadbum <54482247+silkroadbum@users.noreply.github.com>
2023-05-02 17:30:21 +0600

    Добавил проверку на формат json при отправке и блокировку кнопки отправить данные при отсутствии данных в поле textarea

--- src/components/textarea/textarea.js
+++ src/components/textarea/textarea.js
@@ -1,20 +1,43 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { updateList } from '../../store/data/dataSlice';
 
 function Textarea() {
+  const [isDisabled, setIsDisabled] = useState(true);
   const dispatch = useDispatch();
   const { data } = useSelector((state) => state.data);
   const textAreaRef = useRef(null);
 
   const onClickGetData = () => {
-    textAreaRef.current.value = JSON.stringify(data);
+    try {
+      textAreaRef.current.value = JSON.stringify(data);
+      setIsDisabled(false);
+    } catch (error) {
+      alert('Не удалось получить данные!');
+      console.error(error);
+    }
   };
 
   const onClickSendData = () => {
-    const newData = JSON.parse(textAreaRef.current.value);
-    dispatch(updateList(newData));
-    textAreaRef.current.value = '';
+    if (textAreaRef.current.value) {
+      try {
+        const newData = JSON.parse(textAreaRef.current.value);
+        dispatch(updateList(newData));
+        textAreaRef.current.value = '';
+        setIsDisabled(true);
+      } catch (err) {
+        alert('Данные введены не в формате JSON');
+        console.error(err);
+      }
+    }
+  };
+
+  const checkTextArea = ({ target }) => {
+    if (target.value) {
+      setIsDisabled(false);
+    } else {
+      setIsDisabled(true);
+    }
   };
 
   return (
@@ -23,12 +46,13 @@ function Textarea() {
         <button className="textarea__btn" onClick={onClickGetData}>
           Получить данные
         </button>
-        <button className="textarea__btn" onClick={onClickSendData}>
+        <button className="textarea__btn" onClick={onClickSendData} disabled={isDisabled}>
           Загрузить данные
         </button>
       </div>
       <div className="textarea__right-block">
         <textarea
+          onInput={checkTextArea}
           className="textarea__text-field"
           name="json"
           rows="20"
