29532affc86c1745554b624f692a3d0f218ffbb4
silkroadbum <54482247+silkroadbum@users.noreply.github.com>
2023-05-04 14:18:47 +0600

    реализовал загрузку json и csv файлов в textarea

--- src/components/textarea/textarea.js
+++ src/components/textarea/textarea.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { updateList } from '../../store/data/dataSlice';
+import csvtojson from 'csvtojson';
 
 function Textarea() {
   const [isDisabled, setIsDisabled] = useState(true);
@@ -41,6 +42,30 @@ function Textarea() {
     }
   };
 
+  const handleFileUpload = ({ target }) => {
+    const file = target.files[0];
+    const reader = new FileReader();
+    if ((file && file.type === 'text/csv') || (file && file.type === 'application/json')) {
+      reader.onload = () => {
+        let data;
+        if (file.type === 'text/csv') {
+          csvtojson()
+            .fromString(reader.result)
+            .then((json) => {
+              setText(JSON.stringify(json));
+            });
+        } else {
+          data = reader.result;
+        }
+        setText(data);
+        setIsDisabled(false);
+      };
+      reader.readAsText(file);
+    } else {
+      alert('Загрузите файл формата JSON или СVS');
+    }
+  };
+
   return (
     <div className="textarea">
       <div className="textarea__left-block">
@@ -50,6 +75,18 @@ function Textarea() {
         <button className="textarea__btn" onClick={onClickSendData} disabled={isDisabled}>
           Загрузить данные
         </button>
+        <div className="textarea__file-block">
+          <label className="textarea__input-label" htmlFor="file">
+            Выберете файл JSON/CVS:
+          </label>
+          <input
+            onChange={handleFileUpload}
+            type="file"
+            className="textarea__input"
+            name="file"
+            id="file"
+          />
+        </div>
       </div>
       <div className="textarea__right-block">
         <textarea
